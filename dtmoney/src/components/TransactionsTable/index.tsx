d924124af62f1def9182d00faf97f0de9785067d
Francelino <daniel.francelino@capgemini.com>
2022-02-19 15:13:50 -0300

    MirageJS

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+    useEffect(() => {
+        fetch('http://localhost:3000/api/transactions')
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }, []);
+
     return (
         <Container>
             <table>
@@ -16,12 +23,6 @@ export function TransactionsTable() {
                 <tbody>
                     <tr>
                         <td className="title">Desenvolvimento de website</td>
-                        <td>R$ 12.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td className="title">Desenvolvimento de website</td>
                         <td className="deposit">R$ 12.000,00</td>
                         <td>Desenvolvimento</td>
                         <td>20/02/2021</td>
@@ -31,11 +32,6 @@ export function TransactionsTable() {
                         <td className="withdraw">- R$ 1.100,00</td>
                         <td>Casa</td>
                         <td>07/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td className="title">Desenvolvimento de website</td>
-                        <td>R$ 12.000,00</td>
-                        <td>20/02/2021</td>
                     </tr>
                 </tbody>
             </table>
