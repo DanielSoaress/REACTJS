12539b4e000239fb0bc712b0333ead87fc0bb560
Francelino <daniel.francelino@capgemini.com>
2022-04-10 23:04:07 -0300

    PRISMIC funcionado

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -9,36 +23,51 @@ export default function Posts() {
             </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
-                        <p>
-                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
-                        </p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
-                        <p>
-                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
-                        </p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
-                        <p>
-                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
-                        </p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
-                        <p>
-                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
-                        </p>
-                    </a>
+                    {
+                        posts.map(post => (
+                            <a key={post.slug} href={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>
+                                    {post.excerpt}
+                                </p>
+                            </a>
+                        ))
+                    }
                 </div>
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query<any>(
+        [Prismic.predicates.at('document.type', 'template-post')],
+        {
+            fetch: ['template-post.title', 'template-post.content'],
+            pageSize: 10,
+        }
+    )
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',
+                {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+        }
+    })
+
+    return {
+        props: {
+            posts
+        }
+    }
 }
