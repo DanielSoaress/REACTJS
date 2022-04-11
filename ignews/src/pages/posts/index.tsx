8250c6490330f9062d85e2fdb139e0219eb9b374
Francelino <daniel.francelino@capgemini.com>
2022-04-10 21:29:21 -0300

    Antes de sofrer

---
+++ ignews/src/pages/posts/index.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
+                        <p>
+                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
+                        </p>
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
+                        <p>
+                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
+                        </p>
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
+                        <p>
+                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
+                        </p>
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Python: linguagem popular com berço na comédia inglesa</strong>
+                        <p>
+                            Ocupando o pódio entre as gigantes do mercado, Python conquistou a programação com sua estrutura madura, referência e humor
+                        </p>
+                    </a>
+                </div>
+            </main>
+        </>
+    );
+}
