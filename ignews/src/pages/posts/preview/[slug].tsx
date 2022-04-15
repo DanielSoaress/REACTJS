ebb473aca5a2a0c38c492c3a3f78e1dbd49df9c8
Francelino <daniel.francelino@capgemini.com>
2022-04-15 00:24:24 -0300

    JAMStack

--- ignews/src/pages/posts/preview/[slug].tsx
+++ ignews/src/pages/posts/preview/[slug].tsx
@@ -1,11 +1,14 @@
-import { GetStaticProps } from "next";
-import { getSession } from "next-auth/react";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import { RichText } from "prismic-dom";
+import Link from "next/link";
 
 import { getPrismicClient } from "../../../services/prismic";
 
 import styles from '../post.module.scss';
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface PostPreviewProps {
     post: {
@@ -17,6 +20,15 @@ interface PostPreviewProps {
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
+    const { data: session } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session?.activeSubscription) {
+            router.push(`/posts/${post.slug}`)
+        }
+    }, [session])
+
     return (
         <>
             <Head>
@@ -27,15 +39,25 @@ export default function PostPreview({ post }: PostPreviewProps) {
                 <article className={styles.post}>
                     <h1>{post.title}</h1>
                     <time>{post.updatedAt}</time>
-                    <div className={`${styles.postContent} ${styles.previewContent}`}
-                     dangerouslySetInnerHTML={{ __html: post.content }} />
+                    <div
+                        className={`${styles.postContent} ${styles.previewContent}`}
+                        dangerouslySetInnerHTML={{ __html: post.content }}
+                    />
+
+                    <div className={styles.continueReading}>
+                        Wanna continue reading?
+                        <Link href="/">
+                            <a >Subscribe now 🤗</a>
+                        </Link>
+                    </div>
+
                 </article>
             </main>
         </>
     );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [],
         fallback: 'blocking'
@@ -64,8 +86,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post
-        }
+        },
+        redirect: 60 * 60 * 24, //24 hours
     }
-
 }
+
 
